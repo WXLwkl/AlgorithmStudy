6f8e2593a7191349e080297ee5641437785d667f
xingl <wangxinglin@dushu365.com>
2022-06-03 12:30:12 +0800

    leetcode946

---
+++ AlgorithmStudy/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AlgorithmStudy
+//
+//  Created by xingl on 2022/6/3.
+//
+
+import Foundation
+
+let valid = Leetcode946.validateStackSequences([1, 2, 3, 4, 5], [4, 3, 5, 1, 2])
+print(valid)
+
